2f8113ce7b870f2712e27ecfd1fcaf74b628941e
Katedudina <dudinaj97@gmail.com>
2018-02-26 21:30:44 +0200

    Add project
    Add project

---
+++ easycook/src/main/java/com/easycook/easycook/EasycookApplication.java
@@ -0,0 +1,12 @@
+package com.easycook.easycook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasycookApplication.class, args);
+	}
+}
---
+++ easycook/src/main/java/com/example/easycook/EasycookApplication.java
@@ -0,0 +1,12 @@
+package com.example.easycook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasycookApplication.class, args);
+	}
+}
