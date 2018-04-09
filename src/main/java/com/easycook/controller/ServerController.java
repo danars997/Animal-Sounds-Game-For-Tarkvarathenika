623304df82c4bbd815060f980818a1ffca2b628f
danars997 <arseny.krutikov@gmail.com>
2018-04-09 18:36:35 +0300

    Created server controller

---
+++ src/main/java/com/easycook/controller/ServerController.java
@@ -0,0 +1,12 @@
+package com.easycook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ServerController{
+    @GetMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
