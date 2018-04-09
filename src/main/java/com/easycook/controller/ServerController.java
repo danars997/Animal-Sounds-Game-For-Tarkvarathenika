60283e543541eda8216afb0b665d41cbf292a10d
danars997 <arseny.krutikov@gmail.com>
2018-04-09 19:32:53 +0300

    Added CSS serving

--- src/main/java/com/easycook/controller/ServerController.java
+++ src/main/java/com/easycook/controller/ServerController.java
@@ -4,6 +4,7 @@ import com.easycook.service.ServerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ServerController{
@@ -13,5 +14,11 @@ public class ServerController{
     @GetMapping(value = "/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping(value = "/css/{cssfile}")
+    public String getCssFile(@PathVariable("cssfile") String cssfile)
+    {
+        return "css/" + cssfile;
     }
 }
